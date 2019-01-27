a0eedf00ea3384e6a9c1596e2facc1bc8f9dd6dc
SINAN COSKUN <EXT01D4575@turkcell.entp.tgc>
2019-01-27 21:22:22 +0300

    DOM Render

--- second-app/src/index.js
+++ second-app/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Force from './Force';
 
+
+/* this render for App 
 ReactDOM.render(<App headerProp="Header Prop From index.js..."
     contentProp="Content Prop From index.js..." />, document.getElementById('root'));
+*/
+
+
+
+ReactDOM.render(<Force />, document.getElementById('root'));
 
